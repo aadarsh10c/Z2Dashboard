5fdafcac758a3e7cb28379426f436a62d5dcf43a
Adarsh Kumar <adarsh010c@gmail.com>
2025-01-23 00:12:15 +0530

    refactor: streamline StaticSidebar component by simplifying props and improving map function

--- src/util/StaticSidebar.jsx
+++ src/util/StaticSidebar.jsx
@@ -1,17 +1,11 @@
 import PropTypes from "prop-types";
 
-export function StaticSidebar({
-  menuItems ,
-  className = "",
-}) {
+export function StaticSidebar({ menuItems, className = "" }) {
   return (
     <aside className={`w-52 bg-white border-r border-gray-200 ${className}`}>
       <nav className="px-2 py-2">
-        {menuItems.map((item, index) => (
-          <div
-            key={item.title}
-            className={`mb-6 ${index === menuItems.length - 1 ? "" : "mb-6"}`}
-          >
+        {menuItems.map((item) => (
+          <div key={item.title} className={`mb-6 `}>
             {item.isHeader ? (
               <div className="px-2 mb-1 text-sm font-semibold text-gray-500">
                 {item.title}
