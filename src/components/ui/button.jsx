b0f723f632eeef95909a7bde4936b960acad9991
adarsh010c <adarsh010c@gmail.com>
2025-01-22 12:31:46 +0530

    added teh feature sidebar

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils"
 
-export const buttonVariants = cva(
+const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
@@ -34,7 +34,7 @@ export const buttonVariants = cva(
   }
 )
 
-export const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
   return (
     (<Comp
@@ -45,3 +45,4 @@ export const Button = React.forwardRef(({ className, variant, size, asChild = fa
 })
 Button.displayName = "Button"
 
+export { Button, buttonVariants }
