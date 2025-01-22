0c088121ed9f92c4d959dd3b9944e344aed58e0c
Adarsh Kumar <adarsh010c@gmail.com>
2025-01-23 00:33:29 +0530

    feat: add action buttons for saved reports, run reports, and new BOM in PartRiskMain header

--- src/pages/PageRisk/PartRiskMain.jsx
+++ src/pages/PageRisk/PartRiskMain.jsx
@@ -4,16 +4,45 @@ import { useLocation } from "react-router";
 import { BreadcrumbWithCustomSeparator } from "@/util/BreadCrumb";
 import { Outlet } from "react-router";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { Save, Play, Plus } from "lucide-react";
+
 export default function PartRiskMain() {
   const { pathname } = useLocation();
   const paths = pathname.split("/").filter(Boolean);
   return (
     <section className="bg-secondary rounded-l-md overflow-hidden p-2">
-      <header className="flex items-center mb-2">
+      <header className="flex items-center justify-between mb-2">
         <BreadcrumbWithCustomSeparator crumbList={paths} />
+        <div className="flex items-center gap-2">
+          <Button
+            variant="outline"
+            className="h-7 text-xs px-1.5  text-primary-foreground rounded-md hover:bg-secondary-foreground hover:text-primary"
+          >
+            <span>
+              <Save />
+            </span>
+            <span>Saved Reports</span>
+          </Button>
+          <Button
+            variant="outline"
+            className="h-7 text-xs px-1.5 text-primary-foreground rounded-md hover:bg-secondary-foreground hover:text-primary"
+          >
+            <span>
+              <Play />
+            </span>
+            <span>Run Reports</span>
+          </Button>
+          <Button className="h-7 text-xs px-1.5 bg-primary text-secondary-foreground rounded-md hover:bg-primary-foreground hover:text-secondary-foreground">
+            <span>
+              <Plus />
+            </span>
+            <span>New BOM</span>
+          </Button>
+        </div>
       </header>
       <Separator />
-      <div className="flex-1 overflow-y-auto bg-secondary overflow-y-auto">
+      <div className="flex-1 bg-secondary overflow-y-auto">
         <Outlet />
       </div>
     </section>
