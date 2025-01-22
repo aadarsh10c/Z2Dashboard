15f7c6a910ddff8c1d8ad73942c19d511b1c1e75
Adarsh Kumar <adarsh010c@gmail.com>
2025-01-23 01:54:53 +0530

    refactor: update MyDataTab component to enhance tab functionality and improve UI consistency; rename component and adjust tab values for better clarity

--- src/pages/PageRisk/SubPages/MyData/MyDataTab.jsx
+++ src/pages/PageRisk/SubPages/MyData/MyDataTab.jsx
@@ -1,24 +1,30 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const commonTabStyles =
-  "relative py-2 px-4 text-sm font-medium transition-all rounded-none data-[state=active]:text-blue-600 bg-secondary shadow-none before:content-[''] before:absolute before:bottom-[-1px] before:left-1/2 before:-translate-x-1/2 before:w-16 before:h-0.5 before:bg-blue-500 data-[state=active]:before:block before:hidden";
+  "relative text-xs py-2 px-4 text-sm font-medium transition-all rounded-none data-[state=active]:text-blue-600 bg-secondary shadow-none before:content-[''] before:absolute before:bottom-[-1px] before:left-1/2 before:-translate-x-1/2 before:w-16 before:h-0.5 before:bg-blue-500 data-[state=active]:before:block before:hidden";
 
-export function TabsDemo() {
+export default function MyDataTab() {
   return (
-    <Tabs defaultValue="account" className="flex-1">
+    <Tabs defaultValue="allFloders" className="flex-1">
       <TabsList className="inline-flex h-9 items-center justify-start w-auto bg-secondary border-b">
-        <TabsTrigger value="account" className={commonTabStyles}>
-          Account
+        <TabsTrigger value="allFolders" className={commonTabStyles}>
+          All Folders
         </TabsTrigger>
-        <TabsTrigger value="password" className={commonTabStyles}>
-          Password
+        <TabsTrigger value="myFolders" className={commonTabStyles}>
+          My Folders
+        </TabsTrigger>
+        <TabsTrigger value="favorites" className={commonTabStyles}>
+          Favorites
         </TabsTrigger>
       </TabsList>
-      <TabsContent value="account" className="p-4">
-        <h1>Account</h1>
+      <TabsContent value="allFolders" className="p-4">
+        <h1>All Folders</h1>
       </TabsContent>
-      <TabsContent value="password" className="p-4">
-        <h1>Password</h1>
+      <TabsContent value="myFolders" className="p-4">
+        <h1>My Folders</h1>
+      </TabsContent>
+      <TabsContent value="favorites" className="p-4">
+        <h1>Favorites</h1>
       </TabsContent>
     </Tabs>
   );
