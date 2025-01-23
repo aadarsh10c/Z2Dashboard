47793d67447ae9f082fb02a81dc335dde8876eed
Adarsh Kumar <adarsh010c@gmail.com>
2025-01-23 18:01:32 +0530

    refactor: update PartRiskMain component layout for improved responsiveness; adjust section and div classes to enhance styling and overflow handling

--- src/pages/PageRisk/PartRiskMain.jsx
+++ src/pages/PageRisk/PartRiskMain.jsx
@@ -3,17 +3,15 @@ import { Outlet } from "react-router";
 import { Separator } from "@/components/ui/separator";
 import PageRiskHeader from "./PageRiskHeader";
 
-
-
 export default function PartRiskMain() {
   const { pathname } = useLocation();
   const paths = pathname.split("/").filter(Boolean);
 
   return (
-    <section className="h-screen bg-secondary rounded-l-md p-2 flex flex-col">
+    <section className="flex flex-col flex-1 h-full bg-secondary rounded-l-md p-2">
       <PageRiskHeader paths={paths} />
       <Separator />
-      <div className="flex-1 bg-secondary">
+      <div className="flex-1 bg-secondary overflow-auto">
         <Outlet />
       </div>
     </section>
